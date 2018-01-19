d0519ae5b2abd19d9f4213afe2e355f433f17cd4
Amber Joy <amber.joy@gmail.com>
2018-01-18 22:48:34 -0600

    Scripts and styles all applied on click

--- awesome.js
+++ awesome.js
@@ -1,9 +1,9 @@
-    // Get the extra calories text element
-    var number = document.getElementsByClassName('energy-remaining-number negative-energy');
+// Get the extra calories text element
+var number = document.getElementsByClassName('energy-remaining-number negative-energy');
 
-    // Slice the minus sign off the negative number
-    var newNum = $(number).text().slice(10);
+// Slice the minus sign off the negative number (I don't know why, but there are 9 spaces before it!)
+var newNum = $(number).text().trim().slice(1);
 
-    // Replaces the negative number with a positive
-    $(number).text(newNum);
-    $(number).append("bonus points");
+// Replaces the negative number with a positive
+$(number).text(newNum);
+$(number).append(" bonus points!");
--- background.js
+++ background.js
@@ -1,9 +1,22 @@
+var buttonDisabled = false;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {
-        file: "awesome.js"
-    }, function() {
-        if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-        }
-    });
+    if (buttonDisabled == false) {
+        // Pretty up the CSS
+        chrome.tabs.insertCSS(tab.id, {
+            file: "awesome.css"
+        });
+        // Awesome up the negative number
+        chrome.tabs.executeScript(tab.id, {
+            file: "awesome.js"
+        }, function() {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError.message);
+            }
+        });
+        // Disable the button - it should only work once!
+        buttonDisabled = true;
+    } else {
+        return;
+    }
 });
