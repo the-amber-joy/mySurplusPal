0edaee3308e16652bd0c24d33ec5a869db101097
Amber Joy <amber.joy@gmail.com>
2018-02-08 00:44:55 -0600

    adds check so extension only works once to update DOM, once to alert, then is disabled unless tab is updated

--- awesome.js
+++ awesome.js
@@ -1,14 +1,13 @@
 // Get the element
-var number = document.getElementsByClassName('energy-remaining-number negative-energy');
-console.log("click!");
+var target = document.getElementsByClassName('energy-remaining-number negative-energy');
 
-if (document.getElementsByClassName('energy-remaining-number negative-energy')[0].textContent.indexOf("bonus") < 0) {
+if (target[0].textContent.indexOf("bonus") < 0) {
     // Slice the minus sign off the negative number (I don't know why, but there are 9 spaces before it!)
-    var newNum = $(number).text().trim().slice(1);
+    var newNum = $(target).text().trim().slice(1);
 
     // Replaces the negative number with a positive
-    $(number).text(newNum);
-    $(number).append(" bonus points!");
+    $(target).text(newNum);
+    $(target).append(" bonus points!");
 } else {
     alert("Awesome score! Have a great day tomorrow!");
 }
--- background.js
+++ background.js
@@ -1,15 +1,21 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // Pretty up the CSS
-    chrome.tabs.insertCSS(tab.id, {
-        file: "awesome.css"
-    });
-    // Awesome up the negative number
-    chrome.tabs.executeScript(tab.id, {
-        file: "awesome.js"
-    }, function() {
-        if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-        }
-    });
-    clickCount ++;
+var clickCounter = 0;
+
+chrome.tabs.onUpdated.addListener(function (tab){
+    clickCounter = 0;
 });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    if (clickCounter < 2) {
+        // Pretty up the CSS
+        chrome.tabs.insertCSS(tab.id, {
+            file: "awesome.css"
+        });
+        // Awesome up the negative number
+        chrome.tabs.executeScript(tab.id, {
+            file: "awesome.js"
+        });
+        clickCounter++;
+    }
+});
+
+
