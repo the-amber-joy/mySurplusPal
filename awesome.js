4de857dbd35fc73224da11420a78485e6486938e
Amber Joy <amber.joy@gmail.com>
2018-02-07 23:55:24 -0600

    some fixes so it runs better

--- awesome.js
+++ awesome.js
@@ -1,9 +1,15 @@
-// Get the extra calories text element
+// Get the element
 var number = document.getElementsByClassName('energy-remaining-number negative-energy');
+console.log("click!");
 
-// Slice the minus sign off the negative number (I don't know why, but there are 9 spaces before it!)
-var newNum = $(number).text().trim().slice(1);
+if (document.getElementsByClassName('energy-remaining-number negative-energy')[0].textContent.indexOf("bonus") < 0) {
+    // Slice the minus sign off the negative number (I don't know why, but there are 9 spaces before it!)
+    var newNum = $(number).text().trim().slice(1);
 
-// Replaces the negative number with a positive
-$(number).text(newNum);
-$(number).append(" bonus points!");
+    // Replaces the negative number with a positive
+    $(number).text(newNum);
+    $(number).append(" bonus points!");
+} else {
+    alert("Awesome score! Have a great day tomorrow!");
+}
+
--- background.js
+++ background.js
@@ -1,22 +1,15 @@
-var buttonDisabled = false;
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-    if (buttonDisabled == false) {
-        // Pretty up the CSS
-        chrome.tabs.insertCSS(tab.id, {
-            file: "awesome.css"
-        });
-        // Awesome up the negative number
-        chrome.tabs.executeScript(tab.id, {
-            file: "awesome.js"
-        }, function() {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-            }
-        });
-        // Disable the button - it should only work once!
-        buttonDisabled = true;
-    } else {
-        return;
-    }
+    // Pretty up the CSS
+    chrome.tabs.insertCSS(tab.id, {
+        file: "awesome.css"
+    });
+    // Awesome up the negative number
+    chrome.tabs.executeScript(tab.id, {
+        file: "awesome.js"
+    }, function() {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+        }
+    });
+    clickCount ++;
 });
