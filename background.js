ed3b14c689c56f8a1b9b8b34a27faea23266e3e8
Amber Joy <amber.joy.dev@gmail.com>
2018-02-14 00:57:45 -0600

    enables button only when active tab is pointed at MFP

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
-  ////////////////////////////////
- ///// FUNCTION DEFINITIONS /////
-////////////////////////////////
+  ///////////////////////////////////////////
+ ///// VARIABLE & FUNCTION DEFINITIONS /////
+///////////////////////////////////////////
+
+var enableBasedOnLocation = function() {
+    getCurrentTab().then(function(tab){
+        if (tab.url !== "http://www.myfitnesspal.com/") {
+            chrome.browserAction.disable(tab.id);
+        } else if (tab.url == "http://www.myfitnesspal.com/") {
+            chrome.browserAction.enable(tab.id);
+        }   
+    });
+    getUpdatedTab();
+}
 
 // manipulate the DOM and enable the popup for any subsequent clicks
 var awesomeItUp = function(request) {
@@ -13,8 +24,10 @@ var awesomeItUp = function(request) {
         });
         chrome.browserAction.setPopup({popup: "popup.html"});
     }
+}
 
-    if (popupOpen.true){
+var disablePopupAfterOpening = function(request) {
+    if(request.popupOpen) {
         chrome.browserAction.setPopup({popup: ""});
     }
 }
@@ -28,9 +41,35 @@ var setPopupAgain = function(request) {
     }
 }
 
-  /////////////////////////////////////
- ///// BROWSER & RUNTIME ACTIONS /////
-/////////////////////////////////////
+function getCurrentTab(){
+    return new Promise(function(resolve){
+      chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      }, function(tabs) {
+        resolve(tabs[0]);
+      });
+    });
+}
+
+function getUpdatedTab(tabId, changeInfo, tab) {
+    if (changeInfo.url !== undefined) {
+        if (changeInfo.url !== "http://www.myfitnesspal.com/") {
+            chrome.browserAction.disable(tabId);
+        } else if (changeInfo.url == "http://www.myfitnesspal.com/") {
+            chrome.browserAction.enable(tabId)
+        }
+    } else { 
+        return; 
+    }
+}
+
+  ///////////////////////////
+ ///// BROWSER ACTIONS /////
+///////////////////////////
+
+// Listen for current location, enable button only on MFP
+chrome.tabs.onActivated.addListener(enableBasedOnLocation);
 
 // On click, check the status of the page and determine which action to take
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -42,7 +81,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 // This makes sure the newly assigned popup doesn't hijack the click listener event
-// The automatic disabling prevents the popup from remaining the default click action on subsequent page visits within a short time
+// It prevents the popup from remaining the default click action on subsequent page visits within a short time
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if(request.popupOpen) {
         chrome.browserAction.setPopup({popup: ""});
--- logic.js
+++ logic.js
@@ -2,9 +2,9 @@
 var target = document.getElementsByClassName('energy-remaining-number negative-energy');
 
 // Trigger message to manipulate the DOM if untouched, or set popup if already changed
-if (target[0].textContent.indexOf("bonus points") < 0) {
+if (target && target[0].textContent.indexOf("bonus points") < 0) {
     chrome.runtime.sendMessage({message: "awesome it up"});
 } 
-else if (target[0].textContent.indexOf("bonus points") > -1) {
+else if (target && target[0].textContent.indexOf("bonus points") > -1) {
     chrome.runtime.sendMessage({message: "set popup"});   
 }
